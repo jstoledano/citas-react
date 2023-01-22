efd17eb8ea44de49959385f1fac2b6737a3d6e2e
Javier Sanchez Toledano <2502353+jstoledano@users.noreply.github.com>
2023-01-22 13:42:50 -0600

    Se quita el useEffect de ListadoPacientes

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,13 +1,6 @@
-import { useEffect } from "react"
 import { Paciente } from "./Paciente.jsx"
 
 const ListadoPacientes = ({pacientes, setPaciente}) => {
-
-  useEffect(() => {
-    if (pacientes.length > 0) {
-      console.log(`Se creo el componente ${Paciente.name}`)
-    }
-  }, [pacientes])
 
   return (
     <div className={'md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll'}>
